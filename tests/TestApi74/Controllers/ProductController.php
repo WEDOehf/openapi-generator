e5448a1a31af1b4377e433541e6771b24b965cf3
Dalibor Korpar <dalibor.korpar@gmail.com>
2020-05-25 12:53:15 +0200

    support for php 7.4 typed properties

---
+++ tests/TestApi74/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php declare (strict_types = 1);
+
+namespace Wedo\OpenApiGenerator\Tests\TestApi74\Controllers;
+
+use DateTime;
+use Wedo\OpenApiGenerator\Tests\TestApi74\Responses\ProductResponse;
+
+class ProductController extends BaseController
+{
+
+	/**
+	 * @param bool $extra
+	 */
+	public function get(int $id): ProductResponse
+	{
+		$response = new ProductResponse();
+		$response->id = $id;
+		$response->expires_at = new DateTime();
+		$response->price = 129900;
+		$response->published = true;
+		$response->title = 'Product ' . $id . ' cost 1299,00';
+		return $response;
+	}
+
+}
---
+++ tests/TestApi74/Requests/EditProfileRequest.php
@@ -0,0 +1,12 @@
+<?php declare (strict_types = 1);
+
+namespace Wedo\OpenApiGenerator\Tests\TestApi74\Requests;
+
+class EditProfileRequest extends BaseRequest
+{
+
+	public string $first_name;
+
+	public string $last_name;
+
+}
